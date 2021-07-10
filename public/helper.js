298856d3a1c979b93bfca40167a3671d94ad639f
Brandon Brum <BrandonBrum@gmail.com>
2021-07-10 16:35:39 -0600

    Changed up the folder structure to remove the unecessary webserver. Added a prototype of the main concept. Ignored workspace specific files.

--- public/helper.js
+++
@@ -1,7 +0,0 @@
-// eslint-disable-next-line no-unused-vars
-const getRandomFrom = arr => {
-  if (typeof (arr[0]) === 'undefined') { arr = Object.values(arr) }
-  return arr[Math.floor(Math.random() * arr.length)]
-}
-// eslint-disable-next-line no-unused-vars
-const mod = (n, m) => ((n % m) + m) % m
